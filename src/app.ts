513b2f7c46d9cb8d6d729c5bdb1add8e2d70fc5b
Nicolas Acero <nikolasg77@gmail.com>
2021-09-22 15:58:54 -0500

    primer commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import axios from 'axios'
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response): void => {
+    res.send('The sedulous hyena ate the antelope!!');
+});
+
+app.listen(port, () => {
+    return console.log(`server is listening on  ${port}`);
+});
