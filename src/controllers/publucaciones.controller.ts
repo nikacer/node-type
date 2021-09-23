c4cead0d64882abc11dcf202be242e610818ccc5
telozano <teolibano@gmail.com>
2021-09-23 16:49:58 -0500

    * ultimos

--- src/controllers/publucaciones.controller.ts
+++ src/controllers/publucaciones.controller.ts
@@ -16,10 +16,55 @@ export default class Publicaciones {
             })
         }).catch(err => res.status(500).send(err))
     }
+    public listarUnaPublicacion(req: Request, res: Response) {
+        let {id} = req.params
+        const path= process.env.path+'/'+req.params.id
+        console.log(path)
+        axios({ url: process.env.path, method: 'GET', params: {id} }).then(({ data }: UPublicacion) => {
+            res.status(200).send({
+                data: data.map(item1 => {
+                    return {
+                        usuario: item1.userId,
+                        id: item1.id,
+                        titulo: item1.title,
+                        cuerpo: item1.body
+                    }
+                })
+            })
+        }).catch(err => res.status(500).send(err))
+    }
+
+    public listarComentarios(req: Request, res: Response) {
+        console.log(req.params.id)
+        console.log(req.params.comentarios)
+        const path= process.env.path+'/'+req.params.id+'/'+req.params.comentarios
+        console.log(path)
+        axios({ url: path, method: 'GET'}).then(({ data }: IComentarios) => {
+            res.status(200).send({
+                data: data.map(item2 => {
+                    return {
+                        postId: item2.postId,
+                        id: item2.id,
+                        nombre: item2.name,
+                        correo: item2.email,
+                        cuerpo: item2.body
+                    }
+                })
+            })
+        }).catch(err => res.status(500).send(err))
+    }
 }
 
 interface IPublicaciones extends AxiosResponse {
     data: Opublicaciones[]
+}
+
+interface UPublicacion extends AxiosResponse {
+    data: Opublicacion[]
+}
+
+interface IComentarios extends AxiosResponse {
+    data: Ocomentarios[]
 }
 
 interface Opublicaciones {
@@ -29,3 +74,17 @@ interface Opublicaciones {
     body: string
 }
 
+interface Opublicacion {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+interface Ocomentarios {
+    postId: number,
+    id: number,
+    name: string,
+    email: string,
+    body: string
+}
--- src/routers/publicaciones.router.ts
+++ src/routers/publicaciones.router.ts
@@ -5,3 +5,5 @@ export const api: IRouter = express.Router()
 const publicaciones = new Controller()
 
 api.get('/', publicaciones.listarPublicaciones)
+api.get('/:id', publicaciones.listarUnaPublicacion)
+api.get('/:id/:comentarios', publicaciones.listarComentarios)
